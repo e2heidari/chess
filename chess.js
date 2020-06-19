2f1c1a199a299c1f87205b66307fbc6a46fa53fa
Ehsan <Ehsan.heydari@gmail.com>
2020-06-19 14:33:08 -0700

    show 8 line

--- chess.js
+++ chess.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let text = ""
     for (let x = 1; x <= 8; x++) {
-        text = x
-
-        if (text % 2 == 1) {
+        text += x
+        if (x % 2 == 1) {
             text += " # # # # "
         }
         else {
             text += "# # # # "
         }
+        text += "\r\n"
     }
 
-    document.getElementById("chess").innerText += text + "\r\n"
+    document.getElementById("chess").innerText = text
 })
